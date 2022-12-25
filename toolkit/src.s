5427cc5ce0d7f2ba9e8488b7f68a6feadc106248
max <mrom06@ya.ru>
2022-12-25 19:38:59 +0300

    Implement simple calculator

--- toolkit/src.s
+++ toolkit/src.s
@@ -94,196 +94,301 @@ PRINT_STRING: {
 
 # =============================================
 READ_STRING: {
-  # ARGS
-  # 1 string_pointer
-  # 0 size
-  $FUNC 0 2 0
-  $PCONST 0
-  $SVAR 0
+  #     1              0
+  # ARG string_pointer size
+  # RET readed
+  # VAR inpsize
+
+  $FUNC 1 2 1
+  
+  $SVAR 0 ($LARG 0)
 
   LOOP:
-  $LARG 0
-  $JEQ LINEEND 
+  $JLE LINEEND ($LARG 0)
 
-  $LARG 0
-  $DEC
-  $SARG 0
-
-  $PUSH ZX
-  $CALL READ_CHAR
+  $SARG 0 ($DEC ($LARG 0))
+  
+  $CALL READ_CHAR ($PUSH ZX)
   $DUP
 
   $LARG 1
   $DUP
-  $INC
-  $SARG 1
+  $SARG 1 ($INC)
   $STORE
 
-  $PCONST 10
-  $SUB
-  $JEQ END
+  $JEQ END ($ACONST -10) # == '\n'
   $JMP LOOP
 
-  LINEEND:
+  LINEEND: # Wait for new line
   $PUSH ZX
   $CALL READ_CHAR
-  $PCONST 10
-  $SUB
-  $JNE LINEEND
+  $JNE LINEEND ($ACONST -10) # != '\n'
 
 
   END:
+  $SARG 1 ($SUB ($LVAR 0; $LARG 0))
   $RET
 }
 
 
 # =============================================
 READ_OP: {
-  # RET
-  # 0 op
-  $FUNC 0 1 1
+  #     0
+  # RET op
+  # ARG prompt
+  $FUNC 1 1 1
 
+  $SVAR 0 ($LARG 0)
   LOOP:
+  
+  $JEQ NO_PROMPT ($LVAR 0)
+  $CALL PRINT_STRING ($LVAR 0)
+  NO_PROMPT:
 
-  $PLABEL BUF
-  $PCONST 1
-  $CALL READ_STRING
-  $PLABEL BUF
-  $LOAD
+  $CALL READ_STRING ($PLABEL BUF; $PCONST 1)
+  $POP ZX
 
-  $DUP
-  $PCONST 43
-  $SUB
-  $JEQ OP_PLUS
+  $LOAD ($PLABEL BUF) # Load char from buf onto stack
 
-  $DUP
-  $PCONST 45
-  $SUB
-  $JEQ OP_MINUS
+  { $JNE IFNOT ($DUP; $ACONST -43)   # char == +
+    $SARG 0 ($PCONST 1); $RET
+  IFNOT: }
 
-  $PCONST 94
-  $SUB
-  $JEQ OP_XOR
+  { $JNE IFNOT ($DUP; $ACONST -45)   # char == +
+    $SARG 0 ($PCONST 2); $RET
+  IFNOT: }
+
+  { $JNE IFNOT ($DUP; $ACONST -94)   # char == +
+    $SARG 0 ($PCONST 3); $RET
+  IFNOT: }
+
+  $POP zx
 
   $PLABEL UNKNOWN_OPERATION
   $CALL PRINT_STRING
 
   $JMP LOOP
 
-  OP_PLUS:
-  $POP zx
-  $PCONST 1
-  $SARG 0
-  $RET
-
-  OP_MINUS:
-  $POP zx 
-  $PCONST 2
-  $SARG 0
-  $RET
-
-  OP_XOR:
-  $POP zx
-  $PCONST 3
-  $SARG 0
-  $RET
-
   BUF: 0 0
   UNKNOWN_OPERATION: "Unknown operation\n" 0
 }
 
+# ===========================================
+MATCH_CHAR_TO_NUM: {
+  #      1    0
+  # RET  num  is_ok
+  # ARG  char _
+  $FUNC 0 2 2
+
+  $LARG 1
+  
+  $SARG 0 ($PCONST 1)
+  $SARG 1 ($PCONST 0xdead)
+
+  { # Check 0-9 
+    $DUP; $ACONST -48
+    $JLT IFNOT ($DUP)                # >= '0'
+    $JGE IFNOT ($DUP; $ACONST -10)   # <= '9'
+
+    $SARG 1; $RET
+    
+    IFNOT: $POP ZX 
+  }
+
+  { # Check a-f & A-F 
+    $DUP; $ACONST -97
+    
+    # If input is uppercase letter than after -97 result will be negative 0
+    # so we add 32 to match it with a-f
+    $JGE SKIP_UPPER_FIX ($DUP);
+    $ACONST 32
+    SKIP_UPPER_FIX:
+
+    $JLT IFNOT ($DUP)                # >= '0'
+    $JGE IFNOT ($DUP; $ACONST -6)   # <= '9'
+
+    $ACONST 10
+    $SARG 1; $RET
+    
+    IFNOT: $POP ZX 
+  }
+
+  $SARG 0 ($PCONST 0)
+  $RET
+}
+
+
+# ===========================================
+MATCH_NUM_TO_CHAR: {
+  #      1   
+  # RET  char 
+  # ARG  num _
+  $FUNC 0 1 1
+
+  $LARG 0
+  $JGE LETTER ($DUP; $ACONST -10)
+  
+  $ACONST 48
+  $SARG 0
+  $RET
+
+  LETTER:
+  $ACONST 87
+  $SARG 0
+
+  $RET
+}
+
+
+# =============================================
+READ_NUM_16: {
+  #     0        1
+  # ARG prompt
+  # RET num     
+  # VAR read_len cur_read prompt
+
+  $FUNC 3 1 1
+
+  $SVAR 2 ($LARG 0)
+  LOOP:
+  
+  $JEQ NO_PROMPT ($LVAR 2)
+  $CALL PRINT_STRING ($LVAR 2)
+  NO_PROMPT:
+
+  $CALL READ_STRING ($PLABEL BUF; $PCONST 5) 
+  
+  $DEC
+  $SVAR 0 # Store
+  
+  $PUSH ZX
+  $JLE FAIL_READ ($LVAR 0) # <= back_point - buf_start
+  $POP ZX
+
+  $SVAR 1 ($PLABEL BUF)
+  $SARG 0 ($PUSH ZX)
+
+  READ_LOOP:
+
+  $LARG 0
+  $SHL; $SHL; $SHL; $SHL
+  $SARG 0
+
+  $CALL MATCH_CHAR_TO_NUM ($LOAD ($LVAR 1); $PUSH ZX)
+  $JEQ FAIL_READ
+  
+  $SARG 0 ($ADD ($LARG 0))
+  $SVAR 1  ($INC ($LVAR 1)) 
+
+  $SVAR 0 ($DEC ($LVAR 0))
+
+  $JLE DONE ($LVAR 0)
+  JMP READ_LOOP
+
+  FAIL_READ:
+  $POP ZX
+  $CALL PRINT_STRING ($PLABEL INVALID_NUMBER)
+  JMP LOOP
+  
+  DONE:
+  $RET 
+
+  BUF: 0 0 0 0 0 0 0
+  INVALID_NUMBER: "Invalid number\n" 0
+}
+
+# =============================================
+
+EXECUTE_OPERATION: {
+  #     2  1 0
+  # ARG op a b
+  # RET te
+  $FUNC 0 3 1
+
+  $LARG 1
+  $LARG 0
+
+  $LARG 2
+
+  { $JNE IFNOT ($DUP; $ACONST -1)   # op plus
+  $POP ZX; $ADD; $SARG 2; $RET
+  IFNOT: }
+
+  { $JNE IFNOT ($DUP; $ACONST -2)   # op sub
+  $POP ZX; $SUB; $SARG 2; $RET
+  IFNOT: }
+
+  { $JNE IFNOT ($DUP; $ACONST -3)   # op plus
+  $POP ZX; $AND; $SARG 2; $RET
+  IFNOT: }
+
+  HALT
+}
+
+# =============================================
+PRINT_NUM16: {
+  #     0
+  # ARG num
+  # RET _
+  # VAR cuw
+  $FUNC 1 1 0
+
+  
+  $SVAR 0 ($DEC ($PLABEL PRINT_BUF_END))
+
+  # Digit left
+  LOOP:
+  $CALL MATCH_NUM_TO_CHAR ($AND ($LARG 0; $PCONST 0xf));
+  $STORE ($LVAR 0)
+
+  $SVAR 0 ($DEC ($LVAR 0))
+  
+  $LARG 0
+  $DUP
+  $SARG 0 ($SHR; $SHR; $SHR; $SHR)
+
+  $JNE LOOP ($LARG 0)
+  
+  $CALL PRINT_STRING ($INC ($LVAR 0))
+  $RET
+
+  PRINT_BUF: "######" PRINT_BUF_END: 0xa 0 0
+}
 
 # =============================================
 
 MAIN: {
   $FUNC 2 0 0
 
-  $PLABEL GREET 
-  $CALL PRINT_STRING
+  $CALL PRINT_STRING ($PLABEL GREET)
 
-  $PCONST 0xDEAD
-  $PCONST 0xDEAD
-  $PCONST 0xDEAD
-  $PCONST 0
-  $CALL READ_OP
+  $CALL READ_OP ($PLABEL ASK_OPERATION)
+  $CALL READ_NUM_16 ($PLABEL ASK_OPERAND1)
+  $CALL READ_NUM_16 ($PLABEL ASK_OPERAND2)
+
+  $CALL EXECUTE_OPERATION
+
+  $CALL PRINT_STRING ($PLABEL RESULT)
+  $CALL PRINT_NUM16
+  
+  $CALL PRINT_STRING ($PLABEL GOODBYE)
+
+  # Wait a little
+  $PUSH ZX; $POP ZX;
+  $PUSH ZX; $POP ZX;
+  $PUSH ZX; $POP ZX;
+  $PUSH ZX; $POP ZX;
 
   HALT
+
+  TEST_BUF: 
 }
 
-# $PLABEL ASK_OPERATION 
-# $CALL PRINT_STRING
+GREET: "Hello, this is simple calculator!\n" 0
+ASK_OPERATION: "Enter operation: " 0
+ASK_OPERAND1:  "Enter operand 1: 0x" 0
+ASK_OPERAND2:  "Enter operand 2: 0x" 0
+RESULT:        "Result:          0x" 0
+GOODBYE:       "Goodbye!\n" 0
 
-
-# $PUSH ZX
-# $CALL READ_CHAR
-# $SVAR 0
-
-# $PUSH ZX
-# $CALL READ_CHAR
-# $POP ZX
-
-# # IS: if(ch == '+')
-# $LVAR 0
-# $ACONST -43 
-# $JNE MAIN__PLUS_ENDIF 
-
-# $PCONST 1 # 1  corresponds to '+'
-# $SVAR 0
-
-# $PLABEL PLUS_OPERATION
-# $CALL PRINT_STRING
-
-# JMP MAIN__OPFOUND
-
-# MAIN__PLUS_ENDIF:
-# # HALT
-
-# $PLABEL UNKNOWN_OPERATION
-# $CALL PRINT_STRING
-# JMP MAIN__ASK_OPERATION
-
-
-# MAIN__OPFOUND:
-# $PCONST 1
-# $CALL WAIT_READ
-# $PCONST 0xDEAD
-# HALT
-
-GREET:
-"Hello, this is simple calculator!"
-0xa 
-0
-
-ASK_OPERATION:
-"Enter required operation: "
-0
-
-
-PLUS_OPERATION:
-"Operation plus"
-0xa
-0
-
-SUB_OPERATION:
-"Operation sub"
-0xa
-0
-
-XOR_OPERATION:
-"Operation xor"
-0xa
-0
-
-
-
-INCORRECT_NUMBER:
-"Incorrect number"
-0xa
-0
-
-
-READ_NUM_BUF:
-0
-0
-0
-0
+DBG: "DEBUG\n" 0
